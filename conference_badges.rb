73984344d0fcabecc7e928bda7311ab42349f480
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-23 22:28:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,12 @@
 # Write your code here.
 
 def batch_badge_creator (arr)
+  count = 0
   arr.each do |person|
-    puts "Hello, my name is #{person}."
+    arr[count] = "Hello, my name is #{person}."
   end
   assign_rooms(arr)
+  return arr
 end
 
 def assign_rooms (arr)
