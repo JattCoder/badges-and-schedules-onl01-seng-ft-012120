62a27accf8e0d89e20eec0f3a0039748b0e70278
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-23 22:38:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,13 +7,12 @@ def batch_badge_creator (arr)
     count += 1
   end
   assign_rooms(arr)
-  return arr
 end
 
 def assign_rooms (arr)
   roomnum = 1
   arr.each do |person|
-    return "Hello #{person}! You'll be assigned to room #{roomnum}!"
+    arr[roomnum-1] "Hello #{person}! You'll be assigned to room #{roomnum}!"
     roomnum += 1
   end
 end
